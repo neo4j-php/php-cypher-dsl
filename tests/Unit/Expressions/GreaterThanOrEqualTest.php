081571a76066b2866369d545fa4a39fa9169c63a
Marijn van Wezel <marijn@wikibase.nl>
2021-08-11 12:24:01 +0200

    Add comparison operators and associated unit tests

---
+++ tests/Unit/Expressions/GreaterThanOrEqualTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Cypher DSL
+ * Copyright (C) 2021  Wikibase Solutions
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace WikibaseSolutions\CypherDSL\Tests\Unit\Expressions;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use WikibaseSolutions\CypherDSL\Expressions\Expression;
+use WikibaseSolutions\CypherDSL\Expressions\GreaterThanOrEqual;
+
+/**
+ * @covers \WikibaseSolutions\CypherDSL\Expressions\GreaterThanOrEqual
+ */
+class GreaterThanOrEqualTest extends TestCase
+{
+	public function testToQuery()
+	{
+		$greaterThanOrEqual = new GreaterThanOrEqual($this->getExpressionMock("a"), $this->getExpressionMock("b"));
+
+		$this->assertSame("(a >= b)", $greaterThanOrEqual->toQuery());
+
+		$greaterThanOrEqual = new GreaterThanOrEqual($greaterThanOrEqual, $greaterThanOrEqual);
+
+		$this->assertSame("((a >= b) >= (a >= b))", $greaterThanOrEqual->toQuery());
+	}
+
+	/**
+	 * Returns a mock of the Expression class that returns the given string when toQuery() is called.
+	 *
+	 * @param string $variable
+	 * @return Expression|MockObject
+	 */
+	private function getExpressionMock(string $variable): Expression
+	{
+		$mock = $this->getMockBuilder(Expression::class)->getMock();
+		$mock->method('toQuery')->willReturn($variable);
+
+		return $mock;
+	}
+}
