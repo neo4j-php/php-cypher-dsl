51e7f439c12eb6f96a85bb2d2d448545d427c419
Marijn van Wezel <marijn@wikibase.nl>
2021-09-29 16:23:42 +0200

    Improve documentation of functions

--- src/Expressions/Functions/Single.php
+++ src/Expressions/Functions/Single.php
@@ -1,27 +1,65 @@
 <?php
 
+/*
+ * Cypher DSL
+ * Copyright (C) 2021  Wikibase Solutions
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 namespace WikibaseSolutions\CypherDSL\Expressions\Functions;
 
 use WikibaseSolutions\CypherDSL\Expressions\Expression;
 
+/**
+ * This class represents the "single()" function.
+ *
+ * @see https://neo4j.com/docs/cypher-manual/current/functions/predicate/#functions-single
+ */
 class Single extends FunctionCall
 {
-    private Expression $variable;
-    private Expression $list;
-    private Expression $predicate;
+	/**
+	 * @var Expression A variable that can be used from within the predicate
+	 */
+	private Expression $variable;
 
-    /**
-     * @param Expression $variable
-     * @param Expression $list
-     * @param Expression $predicate
-     */
+	/**
+	 * @var Expression An expression that returns a list
+	 */
+	private Expression $list;
+
+	/**
+	 * @var Expression A predicate that is tested against all items in the list
+	 */
+	private Expression $predicate;
+
+	/**
+	 * Single constructor. The signature of the "single()" function is:
+	 *
+	 * single(variable :: VARIABLE IN list :: LIST OF ANY? WHERE predicate :: ANY?) :: (BOOLEAN?)
+	 *
+	 * @param Expression $variable  A variable that can be used from within the predicate
+	 * @param Expression $list      An expression that returns a list
+	 * @param Expression $predicate A predicate that is tested against all items in the list
+	 */
     public function __construct(Expression $variable, Expression $list, Expression $predicate)
     {
         $this->variable = $variable;
         $this->list = $list;
         $this->predicate = $predicate;
     }
-
 
     /**
      * @inheritDoc
