caa0b233fc1e5a649af8e68b4876062500a219a6
ghlen <ghlen@pm.me>
2022-01-30 17:43:57 +0100

    added skip clause

---
+++ src/Clauses/SkipClause.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * Cypher DSL
+ * Copyright (C) 2021  Wikibase Solutions
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace WikibaseSolutions\CypherDSL\Clauses;
+
+use WikibaseSolutions\CypherDSL\Types\PropertyTypes\NumeralType;
+
+/**
+ * This class represents a LIMIT clause.
+ *
+ * @see https://neo4j.com/docs/cypher-manual/current/clauses/limit/
+ */
+class SkipClause extends Clause
+{
+    /**
+     * The expression of the LIMIT statement.
+     *
+     * @var NumeralType|null $skip
+     */
+    private ?NumeralType $skip;
+
+    /**
+     * Sets the expression that returns the limit.
+     *
+     * @param NumeralType $limit The limit
+     * @return SkipClause
+     */
+    public function setSkip(NumeralType $limit): self
+    {
+        $this->skip = $limit;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getClause(): string
+    {
+        return "SKIP";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getSubject(): string
+    {
+        if (isset($this->skip)) {
+            return $this->skip->toQuery();
+        }
+
+        return "";
+    }
+}
