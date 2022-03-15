5b62dd8474dcae4f762f345b18662d3a71df7aec
ghlen <ghlen@pm.me>
2022-03-15 01:16:21 +0100

    Created Path

--- src/Patterns/Path.php
+++ src/Patterns/Path.php
@@ -2,63 +2,130 @@
 
 namespace WikibaseSolutions\CypherDSL\Patterns;
 
+use WikibaseSolutions\CypherDSL\PropertyMap;
+use WikibaseSolutions\CypherDSL\Traits\ErrorTrait;
 use WikibaseSolutions\CypherDSL\Traits\PathTrait;
+use WikibaseSolutions\CypherDSL\Types\StructuralTypes\NodeType;
 use WikibaseSolutions\CypherDSL\Types\StructuralTypes\PathType;
-use WikibaseSolutions\CypherDSL\Types\StructuralTypes\StructuralType;
+use WikibaseSolutions\CypherDSL\Types\StructuralTypes\RelationshipType;
+use WikibaseSolutions\CypherDSL\Variable;
+use function is_array;
 
 class Path implements PathType
 {
     use PathTrait;
+    use ErrorTrait;
 
     /** @var Relationship[] */
-    private array $relationships = [];
+    private array $relationships;
+    /** @var Node[] */
+    private array $nodes;
 
-    public function __construct()
+    /**
+     * @param Node|Node[] $nodes
+     * @param Relationship|Relationship[] $relationships
+     */
+    public function __construct($nodes, $relationships = null)
     {
+        self::assertClassOrType('relationships', [Relationship::class, 'array'], $relationships);
+        self::assertClassOrType('nodes', [Node::class, 'array'], $nodes);
+
+        $this->nodes = is_array($nodes) ? array_values($nodes) : [$nodes];
+        $this->relationships = is_array($relationships) ? array_values($relationships) : [$relationships];
     }
 
     public function toQuery(): string
     {
+        if (count($this->nodes) === 0) {
+            return '';
+        }
+
         $cql = '';
         if ($this->getVariable() !== null) {
             $cql = $this->getName()->toQuery() . ' = ';
         }
 
-        foreach ($this->relationships as $relationship) {
-            $cql .= $relationship->getLeft()->toQuery();
-            $cql .= $relationship->toRelationshipQuery();
+        foreach ($this->relationships as $i => $relationship) {
+            if (count($this->nodes) <= $i) {
+                break;
+            }
+            $cql .= $this->nodes[$i]->toQuery();
+            $cql .= $relationship->toQuery();
         }
 
-        if (isset($relationship)) {
-            $cql .= $relationship->getRight()->toQuery();
-        }
+        $cql .= $this->nodes[$i ?? 0]->toQuery();
 
         return $cql;
     }
 
-    public function relationshipTo(StructuralType $pattern): Path
+    /**
+     * Returns the nodes in the path in sequential order.
+     *
+     * @return Node[]
+     */
+    public function getNodes(): array
     {
-        $this->relationships[] = $this->getLatestNode()->relationshipUni($pattern);
+        return $this->nodes;
+    }
+
+    /**
+     * Returns the relationships in the path in sequential order.
+     *
+     * @return Relationship[]
+     */
+    public function getRelationships(): array
+    {
+        return $this->relationships;
+    }
+
+    public function relationship(RelationshipType $relationship, NodeType $node): Path
+    {
+        $this->relationships[] = $relationship;
+        $this->nodes[] = $node;
 
         return $this;
     }
 
-    public function relationshipFrom(StructuralType $pattern): Path
+    public function relationshipTo(NodeType $node, $properties = null, $name = null): Path
     {
-        $this->relationships[] = $this->getLatestNode()->relationshipUni($pattern);
+        $relationship = $this->buildRelationship(Relationship::DIR_RIGHT, $properties, $name);
 
-        return $this;
+        return $this->relationship($relationship, $node);
     }
 
-    public function relationshipUni(StructuralType $pattern): Path
+    public function relationshipFrom(NodeType $node, $properties = null, $name = null): Path
     {
-        $this->relationships[] = $this->getLatestNode()->relationshipUni($pattern);
+        $relationship = $this->buildRelationship(Relationship::DIR_LEFT, $properties, $name);
 
-        return $this;
+        return $this->relationship($relationship, $node);
     }
 
-    private function getLatestNode(): StructuralType
+    public function relationshipUni(NodeType $node, $properties = null, $name = null): Path
     {
-        return $this->relationships[count($this->relationships) - 1]->getRight();
+        $relationship = $this->buildRelationship(Relationship::DIR_UNI, $properties, $name);
+
+        return $this->relationship($relationship, $node);
+    }
+
+    /**
+     * @param array $direction
+     * @param mixed $properties
+     * @param mixed $name
+     *
+     * @return Relationship
+     */
+    private function buildRelationship(array $direction,  $properties, $name): Relationship
+    {
+        self::assertClassOrType('properties', ['array', PropertyMap::class, null], $properties);
+        self::assertClassOrType('name', ['string', Variable::class, null], $name);
+
+        $relationship = new Relationship($direction);
+        if ($properties !== null) {
+            $relationship->withProperties($properties);
+        }
+        if ($name !== null) {
+            $relationship->named($name);
+        }
+        return $relationship;
     }
 }
