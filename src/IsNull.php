161e5cc6ab56a4775e0959054d1c0c6af6959076
ghlen <ghlen@pm.me>
2022-04-21 20:22:13 +0200

    added IsNull

---
+++ src/IsNull.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace WikibaseSolutions\CypherDSL;
+
+use WikibaseSolutions\CypherDSL\Traits\BooleanTypeTrait;
+use WikibaseSolutions\CypherDSL\Types\AnyType;
+use WikibaseSolutions\CypherDSL\Types\PropertyTypes\BooleanType;
+use function sprintf;
+
+/**
+ * Represents the IS NULL comparison operator.
+ *
+ * @see https://neo4j.com/docs/cypher-manual/current/syntax/operators/#query-operators-comparison
+ */
+class IsNull implements BooleanType
+{
+    use BooleanTypeTrait;
+
+    /**
+     * @var AnyType The type to test against null
+     */
+    private AnyType $expression;
+
+    /**
+     * IS NULL constructor.
+     *
+     * @param AnyType $expression The type to test against null.
+     */
+    public function __construct(AnyType $expression)
+    {
+        $this->expression = $expression;
+    }
+
+    /**
+     * Returns the expression to test against null.
+     *
+     * @return AnyType
+     */
+    public function getExpression(): AnyType
+    {
+        return $this->expression;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toQuery(): string
+    {
+        return sprintf("(%s IS NULL)", $this->expression->toQuery());
+    }
+}
