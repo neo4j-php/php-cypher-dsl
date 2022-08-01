1aebce45843197c4fb75998da063d2042385abc8
Marijn van Wezel <marijnvanwezel@gmail.com>
2022-08-01 15:03:56 +0200

    Update php-cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,66 +1,109 @@
 <?php
 
+/*
+ * Cypher DSL
+ * Copyright (C) 2021  Wikibase Solutions
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
-	->in([__DIR__ . '/src', __DIR__ . '/tests'])
-	->name('*php');
+    ->in(__DIR__)
+    ->exclude([
+        '.github',
+        'vendor'
+    ])
+    ->name('*php');
+
+$header = <<<'COPYRIGHT'
+Cypher DSL
+Copyright (C) 2021  Wikibase Solutions
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+COPYRIGHT;
 
 return (new Config())
-	->setRules([
-		'@PSR12' => true,
-		'align_multiline_comment' => true,
-		'array_indentation' => true,
-		'blank_line_before_statement' => [
-			'statements' => [
-				'break',
-				'continue',
-				'declare',
-				'do',
-				'for',
-				'foreach',
-				'if',
-				'include',
-				'include_once',
-				'require',
-				'require_once',
-				'return',
-				'switch',
-				'throw',
-				'try',
-				'while',
-				'yield',
-			],
-		],
-		'concat_space' => ['spacing' => 'one'],
-		'fully_qualified_strict_types' => true,
-		'list_syntax' => [
-			'syntax' => 'short',
-		],
-		'no_unused_imports' => true,
-		'no_useless_else' => true,
-		'no_useless_return' => true,
-		'nullable_type_declaration_for_default_null_value' => true,
-		'ordered_class_elements' => true,
-		'ordered_imports' => true,
-		'phpdoc_align' => [
-			'align' => 'left',
-		],
-		'phpdoc_scalar' => true,
-		'php_unit_method_casing' => [
-			'case' => 'snake_case',
-		],
-		'phpdoc_order_by_value' => [
-			'annotations' => ['covers'],
-		],
-		'phpdoc_no_empty_return' => true,
-		'phpdoc_order' => true,
-		'return_assignment' => true,
-		'self_static_accessor' => true,
-		'simplified_if_return' => true,
-		'trailing_comma_in_multiline' => true
-	])
-	->setFinder($finder)
-	->setCacheFile(__DIR__ . '/.php-cs-fixer.cache')
-	->setUsingCache(true);
+    ->setRules([
+        '@PSR12' => true,
+        'align_multiline_comment' => true,
+        'array_indentation' => true,
+        'blank_line_before_statement' => [
+            'statements' => [
+                'break',
+                'continue',
+                'declare',
+                'do',
+                'for',
+                'foreach',
+                'if',
+                'include',
+                'include_once',
+                'require',
+                'require_once',
+                'return',
+                'switch',
+                'throw',
+                'try',
+                'while',
+                'yield',
+            ],
+        ],
+        'concat_space' => ['spacing' => 'one'],
+        'fully_qualified_strict_types' => true,
+        'header_comment' => [
+            'header' => $header
+        ],
+        'list_syntax' => [
+            'syntax' => 'short',
+        ],
+        'no_unused_imports' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'nullable_type_declaration_for_default_null_value' => true,
+        'ordered_class_elements' => true,
+        'ordered_imports' => true,
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+        'phpdoc_scalar' => true,
+        'php_unit_method_casing' => [
+            'case' => 'snake_case',
+        ],
+        'phpdoc_order_by_value' => [
+            'annotations' => ['covers'],
+        ],
+        'phpdoc_no_empty_return' => true,
+        'phpdoc_order' => true,
+        'return_assignment' => true,
+        'self_static_accessor' => true,
+        'simplified_if_return' => true,
+        'trailing_comma_in_multiline' => true
+    ])
+    ->setFinder($finder);
