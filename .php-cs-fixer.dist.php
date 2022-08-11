dd6104126cec828b850c054e200ecade04d6de94
Marijn van Wezel <marijnvanwezel@gmail.com>
2022-08-11 22:31:56 +0200

    Add risky rules to php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -31,25 +31,16 @@ $finder = Finder::create()
     ->name('*php');
 
 $header = <<<'COPYRIGHT'
-Cypher DSL
-Copyright (C) 2021  Wikibase Solutions
+This file is part of php-cypher-dsl.
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+Copyright (C) 2021-  Wikibase Solutions
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
 COPYRIGHT;
 
 return (new Config())
+	->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true,
         'align_multiline_comment' => true,
@@ -75,35 +66,66 @@ return (new Config())
                 'yield',
             ],
         ],
+		'compact_nullable_typehint' => true,
         'concat_space' => ['spacing' => 'one'],
+		'declare_strict_types' => true,
+		'declare_equal_normalize' => true,
         'fully_qualified_strict_types' => true,
+		'global_namespace_import' => [
+			'import_classes' => true,
+			'import_constants' => false,
+			'import_functions' => false
+		],
         'header_comment' => [
-            'header' => $header
+            'header' => $header,
+			'separate' => 'none'
         ],
         'list_syntax' => [
             'syntax' => 'short',
         ],
+		'logical_operators' => true,
+		'no_superfluous_phpdoc_tags' => true,
+		'no_unset_cast' => true,
         'no_unused_imports' => true,
         'no_useless_else' => true,
         'no_useless_return' => true,
         'nullable_type_declaration_for_default_null_value' => true,
         'ordered_class_elements' => true,
         'ordered_imports' => true,
+		'ordered_interfaces' => true,
+		'ordered_traits' => true,
         'phpdoc_align' => [
             'align' => 'left',
         ],
         'phpdoc_scalar' => true,
+		'phpdoc_order_by_value' => [
+			'annotations' => ['covers'],
+		],
+		'phpdoc_no_empty_return' => true,
+		'phpdoc_order' => true,
+		'php_unit_dedicate_assert' => true,
+		'php_unit_set_up_tear_down_visibility' => true,
         'php_unit_method_casing' => [
-            'case' => 'snake_case',
+            'case' => 'camel_case',
         ],
-        'phpdoc_order_by_value' => [
-            'annotations' => ['covers'],
-        ],
-        'phpdoc_no_empty_return' => true,
-        'phpdoc_order' => true,
+		'php_unit_test_annotation' => [
+			'style' => 'prefix',
+		],
+		'php_unit_test_case_static_method_calls' => [
+			'call_type' => 'this',
+		],
         'return_assignment' => true,
         'self_static_accessor' => true,
+		'single_line_throw' => true,
         'simplified_if_return' => true,
-        'trailing_comma_in_multiline' => true
+		'static_lambda' => true,
+		'strict_comparison' => true,
+		'strict_param' => true,
+        'trailing_comma_in_multiline' => true,
+		'yoda_style' => [
+			'equal' => false,
+			'identical' => false,
+			'less_and_greater' => false,
+		]
     ])
     ->setFinder($finder);
