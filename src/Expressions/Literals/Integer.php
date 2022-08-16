05c2904c3affa6dd724b712f2794629ffe84589d
Wout Gevaert <wout@wikibase.nl>
2022-08-16 17:25:39 +0200

    In Integer::__construct acount for return value false of filter_var

--- src/Expressions/Literals/Integer.php
+++ src/Expressions/Literals/Integer.php
@@ -34,13 +34,19 @@ final class Integer implements IntegerType
         if (!is_int($value) && !is_string($value)) {
             throw new TypeError('An integer should be given as a string or integer, '.gettype($value).' received.');
         }
-        $this->value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+        $parsedValue = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
 
-        if (is_string($value) && $this->value !== $value) {
+        if (false === $parsedValue) {
            throw new TypeError(
-               'A non-integer string has been provided: "'.$value.'", should be something like "'.$this->value.'".'
+               'A non-integer string has been provided: "'.$value.'".'
+           );
+        } elseif (is_string($value) && $parsedValue !== $value)) {
+           throw new TypeError(
+               'A non-integer string has been provided: "'.$value.'", should be something like "'.$parsedValue.'".'
            );
         }
+
+        $this->value = $parsedValue;
     }
 
     /**
